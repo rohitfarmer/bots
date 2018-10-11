22daaccfe39aa937166ce8115f6210bef139c81d
Rohit Farmer <rohit.farmer@gmail.com>
2018-10-11 01:44:03 +0000

    Update

---
+++ experiences1o1/README.md
@@ -0,0 +1,13 @@
+# Experiences1o1
+
+Bots for automating social networks related to [https://experiences1o1.carrd.co/](https://experiences1o1.carrd.co/).
+
+**retweet.py**: At every run fetch 250 recent tweets from the home timeline and retweet the ones that are already retweeted by others more than a certain number of times. Skip if the tweet has already been retweeted by the script in the previous run.
+
+**follow.py**: Follow the users that have followed experiences1o1 and contain certain keyword(s) in their bio.
+
+**unfollow.py**: Unfollow the users that are not following experiences1o1 anymore.
+
+**tweet_motivational_quotes.py**: Tweet a motivational quote from a file once a day.
+
+
--- experiences1o1/tweet_motivational_quotes.py
+++ experiences1o1/tweet_motivational_quotes.py
@@ -1,23 +1,51 @@
-#!/home/rohitfar/public_html/cgi-bin/anaconda3/bin/python3
+#!/usr/bin/python3
 
-import tweepy, os
+'''
+Purpose: To tweet a motivational quote from a file periodically.
+Author: Rohit Farmer
+'''
 
-#OAuth authentication
+# Standard library
+import logging
+import datetime
+import os
 
-#creating an object
+# External library.
+import tweepy
+
+# Create a log file.
+logging.basicConfig(filename='.log/tweet_motivation.log',level=logging.INFO)
+
+# OAuth authentication.
+with open('../../cred/exp.txt', 'r') as f: # Reading the credentials from a text file.
+    creds = f.readlines()
+    consumer_key = creds[0].rstrip()
+    consumer_secret = creds[1].rstrip()
+    access_token = creds[2].rstrip()
+    access_token_secret = creds[3].rstrip()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+# Create tweepy object.
 api = tweepy.API(auth)
 
-f = open('motivationQuotes.txt', 'r')
-ft = open('temp.txt','w')
-quote = f.readline().rstrip()
-print(quote)
+try :
+    f = open('motivationQuotes.txt', 'r')
+    ft = open('temp.txt','w')
+    quote = f.readline().rstrip()
+    logging.info(quote)
+except Exception as ex:
+    logging.info(ex.message)
 
 for i in f :
-	ft.write(i)
+    ft.write(i)
 
 os.remove('motivationQuotes.txt')
 os.rename('temp.txt', 'motivationQuotes.txt')
 
-#Update status
-api.update_status(quote)
+# Update status.
+try:
+    api.update_status(quote)
+except Exception as ex:
+    logging.info(ex.message)
 
